8090745ce1301a21d5581fdac3ee5513c50a20e8
Abhinav Ashutosh <ashutoshabhinavbachra0009@gmail.com>
2026-09-09 21:20:33 +0530

    provided some fixes

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 
 echo "Starting services on Render with PORT=${PORT:-5500}"
 
+# IMPORTANT: Disable agent's internal HTTP server
+export LIVEKIT_AGENT_HTTP_PORT=""
+export LIVEKIT_AGENT_PORT="0"
+
 # Start both services in the background
 echo "Starting HTTP server on port ${PORT:-5500}..."
 python frontend/serve.py &
